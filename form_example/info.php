4b2b330f92e9975a3797d70073526b246e1e3a97
trentransom <116039236+trentransom@users.noreply.github.com>
2023-01-18 12:41:19 -0500

    working with forms

--- form_example/info.php
+++ form_example/info.php
@@ -1,8 +1,8 @@
 <?php
   $connect = mysql_connect('localhost', 'root', 'paperantvivaldi'); 
   if (!connect) {'Connection Failed:'.mysql_error());
-  {mysql_select_db('studentdb', $connect);
-  $user_info = "insert into class101 (to, say) values ('$_POST[]','$_POST[email]')";
+  {mysql_select_db('halamandtrent', $connect);
+  $user_info = "insert into guests (name, email) values ('$_POST[name]','$_POST[email]')";
   if (!mysql_query($user_info, $connect)){die('Error:'.mysql_error());}
   echo "Your information was added to the database.";
   mysql_close($connect);
